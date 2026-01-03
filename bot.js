da89f87c4adc43f5d40c974ea6ff2ac808ee03ae
ACE <jokeacearooni@gmail.com>
2026-01-03 01:27:47 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,6 @@ const client = new Client({
 require("./ticket.js")(client); // load ticket system
 const AutoMod = require("./automod.js");
 AutoMod(client);
-require("./verify-bloxlink.js")(client);
 let warnings = {};
 const WARN_FILE = "./warnings.json";
 
